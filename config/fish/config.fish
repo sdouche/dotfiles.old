fb746e1b88744aa9b37b1ecd4600c8d4176bea8e
Sébastien Douche <sdouche@gmail.com>
2014-12-21 16:07:51 +0100

    Add some Fish aliases

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,4 @@
+
 # Path to your oh-my-fish.
 set fish_path $HOME/.oh-my-fish
 
@@ -7,7 +8,7 @@ set fish_theme bobthefish
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
 # Example format: set fish_plugins autojump bundler
-set fish_plugins jump pyenv tmux peco better-alias
+set fish_plugins jump pyenv tmux peco better-alias 
 
 # Path to your custom folder (default path is $FISH/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
@@ -18,3 +19,7 @@ set fish_plugins jump pyenv tmux peco better-alias
 # Set paths
 set -x GOPATH "$HOME/src/gocode"
 set -x PATH $PATH "$HOME/$GOPATH/bin" "$HOME/bin/go_appengine/"
+
+# Alias
+alias tiga='tig --all'
+alias gitk='gitk --all'
